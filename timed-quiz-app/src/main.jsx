949f1488a1e762b392893f40b4f588067dd97512
Naren <kumarnarendiran2000@gmail.com>
2025-03-30 23:22:09 +0530

    remove commented-out route for ReadOnlyDashboard in main application

--- timed-quiz-app/src/main.jsx
+++ timed-quiz-app/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/publicAdminViewOnlyDashboard" element={<ReadOnlyDashboard />} />
+        {/* <Route path="/publicAdminViewOnlyDashboard" element={<ReadOnlyDashboard />} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
