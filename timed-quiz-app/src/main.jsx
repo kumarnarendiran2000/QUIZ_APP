ed1ab50918eaa5a2b7d29f70ff4b8b51d263218b
Naren <kumarnarendiran2000@gmail.com>
2025-07-04 23:56:57 +0530

    Remove unused ReadOnlyDashboard import from main.jsx

--- timed-quiz-app/src/main.jsx
+++ timed-quiz-app/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ReadOnlyDashboard from "./components/PublicAdminDashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
